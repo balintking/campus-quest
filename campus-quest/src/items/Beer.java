76f0569eb7679333dd0c921f495652d43a4b83f4
Bálint Király <balintkiraly.dev@gmail.com>
2024-04-26 22:37:49 +0200

    Implement Beer class

--- campus-quest/src/items/Beer.java
+++ campus-quest/src/items/Beer.java
@@ -1,40 +1,46 @@
 package items;
 
-import utility.Logger;
+import characters.Person;
+import map.Room;
 
+/**
+ * Represents a Beer item that, when activated by a student,
+ * provides protection against instructors for three rounds.
+ */
 public class Beer extends Item {
 
     public Beer() {
-        super();
-        lifetime = 2;
+        super(null, null, 3, false);
+
     }
 
     /**
-     * The item gets notified about a teacher threat, and it offers its protection againts it if it is active.
+     * Constructor for Beer. Sets life to 3 and active to false by default.
+     */
+    public Beer(Person owner, Room room) {
+        super(owner, room, 3, false);
+    }
+
+    /**
+     * Activates the item and calls dropRandomItem on owner.
+     */
+    @Override
+    public void activate() {
+        active = true;
+        if (owner != null) {
+            owner.dropRandomItem();
+        }
+    }
+
+    /**
+     * The item gets notified about a teacher threat, and it offers its protection against it
+     * with 0 priority if it is active.
      */
     @Override
     public void teacherThreat() {
-        Logger.logCall("teacherThreat", "void");
-        if (isActive) {
+        if (active) {
             owner.teacherProtection(this, 0);
         }
-        Logger.logReturn();
     }
-
-    /**
-     * When time passes, the lifetime of the item decreases, if it hits 0, it gets destroyed.
-     */
-    @Override
-    public void tick() {
-        Logger.logCall("tick", "void");
-        if (isActive) {
-            lifetime--;
-        }
-        if (lifetime == 0) {
-            destroy();
-        }
-        Logger.logReturn();
-    }
-
 }
 

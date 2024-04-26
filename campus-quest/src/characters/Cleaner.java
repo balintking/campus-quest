45655bc2cb7a9c9cc1c035a92460f23471bbdfd8
Captainjan <cardinael.jan.maerten@gmail.com>
2024-04-26 20:25:29 +0200

    cleaning bool added to avoid typecheck in room

--- campus-quest/src/characters/Cleaner.java
+++ campus-quest/src/characters/Cleaner.java
@@ -4,6 +4,8 @@ import items.Item;
 
 public class Cleaner extends Person{
 
+    boolean cleaning = false;
+
     @Override
     public void pickup(Item item) {
 
@@ -12,5 +14,12 @@ public class Cleaner extends Person{
     @Override
     public void teacherAttack() {
 
+    }
+
+    public void initClean(){
+        cleaning = true;
+//      Code can come here if the impl/map branch is merged
+
+        cleaning = false;
     }
 }

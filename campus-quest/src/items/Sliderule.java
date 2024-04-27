403f447c8956d768d74ce4d1f97f21d994d03b8b
Bálint Király <balintkiraly.dev@gmail.com>
2024-04-27 02:42:45 +0200

    Implement Sliderule class

--- campus-quest/src/items/Sliderule.java
+++ campus-quest/src/items/Sliderule.java
@@ -1,26 +1,47 @@
 package items;
 
 import characters.Person;
-import utility.Logger;
+import map.Room;
 
+/**
+ * Represents the Slide rule artifact, the central objective in the game.
+ */
 public class Sliderule extends Item {
+
+    /**
+     * Indicates whether the item is a fake or a genuine version.
+     */
+    private final boolean fake;
+
+    /**
+     * Constructor for SlideRule. Sets life to 1 and active to false by default.
+     */
+    public Sliderule(Person owner, Room room, boolean fake) {
+        super(owner, room, 1, false);
+        this.fake = fake;
+    }
+
+    /**
+     * This item cannot be activated
+     */
+    @Override
+    public void activate() { /* This item cannot be activated */ }
 
     /**
      * Called whenever picked up.
      * Notifies person that they picked the SlideRule up, so they can act accordingly.
-     * @param ownerInput
      */
     @Override
-    public void setOwner(Person ownerInput) {
-        Logger.logCall("setOwner", new Object[]{ownerInput}, "void");
-        owner = ownerInput;
-        if(owner != null){
+    public void setOwner(Person owner) {
+        this.owner = owner;
+        if(owner != null && !fake){
             owner.slideRuleNotification(this);
         }
-        Logger.logReturn();
     }
-    @Override
-    public void destroy(){
 
-    }
+    /**
+     * Time does not affect the state of this Item
+     */
+    @Override
+    public void tick() { /* Time does not affect the state of this Item */ }
 }
